8e4b1853452e14f555b03c20cf74ea6782097f1f
SOME <sourbar2001@gmail.com>
2023-03-09 15:41:50 -0800

    task 1 commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,1 +1,22 @@
-#include "
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *left-child  = malloc(sizeof(binary_tree_t));
+	if (parent == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		if(left-child == NULL)
+		{
+			return NULL;
+		}
+		else
+		{
+			left-child = binary_tree_node(parent->left,value);
+		}
+	}
+}
